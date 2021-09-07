ca75493db30264482197f6f87e632c3179549846
Mariaalice1 <85592927+Mariaalice1@users.noreply.github.com>
2021-09-07 09:20:43 -0300

    Update principal.py
    Meio do trabalho

--- principal.py
+++ principal.py
@@ -1,8 +1,20 @@
 from src.classes.Cidade import cidade
+import json
 
 sair = False
+arquivo = open("./        ,"r") 
+lista_cidades = json.loads(arquivo)
 while sair == False:
-     print("repetindo")
+
+    nome_cidade = input("Digite o nome da cidade:")
+    populaçao_cidade = int(input("Digite a população da cidade:")))
+    sigla_estado = input("Digite a sigla do estado:")
+    nome_estado = input("Digite o nome do estado")
+
+    uf = {"sigla": sigla_estado, "nome": nome_estado}
+    nova_cidade = Cidade(nome_cidade, populacao_cidade,uf)
+     
+    lista_cidades.append(nova_cidade)
 
 resposta = input("Deseja cadastrar outra cidade?")
 
@@ -11,3 +23,5 @@ while resposta.upper() != "S" and resposta.upper() != "N":
     resposta =  input('Deseja cadastrar outra cidade?')
 if resposta.upper () == "N":
     sair = True
+
+print(lista_cidades)
